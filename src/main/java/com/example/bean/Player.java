f3a06c9d29ab2e0aaa44f8335911a70c0a74d697
Anatolie Lupacescu <anatolie.lupacescu@gamesys.com.uk>
2016-01-27 13:17:27 +0000

    added procfile

--- src/main/java/com/example/bean/Player.java
+++ src/main/java/com/example/bean/Player.java
@@ -5,14 +5,24 @@ import org.eclipse.jetty.websocket.api.Session;
 public class Player {
 
     public final transient Session session;
-    public final String name;
-    public final int color;
 
+    public String name;
+    public int color;
     public boolean ready;
+
     private int cellCount;
 
-    public Player(Session session, int color, String name) {
+    public Player(Session session) {
         super();
+        this.session = session;
+    }
+
+    public Player(Session session, String name) {
+        this.session = session;
+        this.name = name;
+    }
+
+    public Player(Session session, int color, String name) {
         this.session = session;
         this.color = color;
         this.name = name;
@@ -34,16 +44,26 @@ public class Player {
         this.cellCount = cellCount;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
 
         Player player = (Player) o;
 
-        if (!session.equals(player.session)) return false;
+        if (!session.equals(player.session))
+            return false;
         return name.equals(player.name);
-
     }
 
     @Override
